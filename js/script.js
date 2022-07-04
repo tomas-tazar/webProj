5126c79c4c7166daa10a6dd8c71ac5d1aa122349
Tomas Tazar <tomas.tazar1337@gmail.com>
2022-07-05 01:35:26 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -5,4 +5,16 @@ function openNav() {
 function closeNav() {
   document.getElementById("popout").style.width = "0";
 }
+window.onload = function() {
+  var reloading = sessionStorage.getItem("reloading");
+  if (reloading) {
+      sessionStorage.removeItem("reloading");
+      document.getElementById('cube').dispatchEvent(new MouseEvent('mouseover', { 'bubbles': true }));
 
+  }
+}
+
+function reloadP() {
+  sessionStorage.setItem("reloading", "true");
+  document.location.reload();
+}
