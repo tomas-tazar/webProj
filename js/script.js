bbaea9153a75b1b70ae528cd74d00d69bc88bdc3
Tomas Tazar <tomas.tazar1337@gmail.com>
2022-07-05 14:51:30 +0200

    relocation

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ function openNav() {
 function closeNav() {
   document.getElementById("popout").style.width = "0";
 }
+function cube1() {
+  document.getElementById('cube').dispatchEvent(new MouseEvent('mouseover', { '': true }));
+}
+
 window.onload = function() {
   var reloading = sessionStorage.getItem("reloading");
   if (reloading) {
       sessionStorage.removeItem("reloading");
-      document.getElementById('cube').dispatchEvent(new MouseEvent('mouseover', { 'bubbles': true }));
+      cube1();
 
   }
 }
