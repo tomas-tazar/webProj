b4ec934829c66ff99209f7707a8545e444085b02
Tomas Tazar <tomas.tazar1337@gmail.com>
2022-08-03 21:02:29 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -22,3 +22,33 @@ function reloadP() {
   sessionStorage.setItem("reloading", "true");
   document.location.reload();
 }
+
+function myFunction() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+
+function myFunction3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
+
+function myFunction4() {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+}
+
+function myFunction5() {
+  var popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show");
+}
+
+function myFunction6() {
+  var popup = document.getElementById("myPopup6");
+  popup.classList.toggle("show");
+}
